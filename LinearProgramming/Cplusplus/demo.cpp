d09e2b351865067173986ff0961b122cd01682d9
khoaxuantu <ngxktuzkai2000@gmail.com>
2022-06-13 00:23:48 +0800

    Update

--- LinearProgramming/Cplusplus/demo.cpp
+++ LinearProgramming/Cplusplus/demo.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <vector>
 
+#include "absl/flags/flag.h"
+#include "ortools/base/init_google.h"
+#include "ortools/base/logging.h"
+#include "ortools/base/logging_flags.h"
 #include "ortools/linear_solver/linear_solver.h"
 
 #define ITEMS_NUM 5
 
 using namespace std;
+using namespace operations_research;
 
 /*
 	Items = 5
@@ -12,13 +18,15 @@ using namespace std;
 	Value = [3, 6, 5, 10, 8]
 	Weight = [2, 7, 1, 3, 4]
 */
+vector<int> value = {3, 6, 5, 10, 8};
+vector<int> weight = { 2, 7, 1, 3, 4 };
 
 void LPsolving()
 {
-	unique_ptr<MPSolver> solver(MPSolver::CreateSolver("SCIP"));
+	unique_ptr<MPSolver> solver(MPSolver::CreateSolver("GLOP"));
 	if (!solver)
 	{
-		LOG(WARNING) << "SCIP is not available.";
+		LOG(WARNING) << "GLOP is not available.";
 		return;
 	}
 
